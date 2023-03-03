a54b9698002042df5c517dd1ec941a5b3e5cf437
Victoria McCrady <spartanginger@gmail.com>
2023-03-03 09:52:35 -0800

    seed data complete

---
+++ seedData/seedData.js
@@ -0,0 +1,91 @@
+const UserData = [
+    {
+        name: "Twitch",
+        email: "heheh@ZaunSewerDept.gov"
+    },
+    {
+        name: "Brie_a_Gouda_Person",
+        email: "softieCheese@gmail.com"
+    },
+    {
+        name: "Cheesy Not Corny34",
+        email: "Ilovepuns@yahoo.com"
+    }
+    
+];
+
+const BoardData = [
+    {
+        title: "my cheese",
+        description: "its MINE, I LICKED it."
+    },
+    {
+        title: "French Cheeses",
+        description: "a board that pairs especially well with fruit"
+    },
+    {
+        title: "secret board",
+        description: "a private board for planning my wife's cheese themed birthday party"
+    },
+    {
+        title: "smells",
+        description: "These cheeses smell really good when left out in the sun for a while"
+    },
+    {
+        title: "friend cheese",
+        description: "a board full of cheeses my friends like"
+    }
+]
+
+const CheeseData = [
+    {
+        type: "Spoiled Milk",
+        description: "Milk thats been introduced to the sun",
+        rating: 0
+    },
+    {
+        type: "Yogurt Mold",
+        description: "my last whisper will be 'yogurt mold'",
+        rating: 0
+    },
+    {
+        type: "Blue Cheese",
+        description: "finally ripe!",
+        rating: 0
+    },
+    {
+        type: "Brie",
+        description: "Typically made with cow's milk, brie is known for its soft, edible rind and smooth, creamy interior.",
+        rating: 5
+    },
+    {
+        type: "Asiago",
+        description: "Pressed asiago is a fresh, semi-soft cheese that is sweet and nutty with a creamy texture",
+        rating: 3
+    },
+    {
+        type: "Provolone",
+        description: "an aged pasta filata (stretched-curd) cheese originating in Campania",
+        rating: 5
+    },
+    {
+        type: "Cambert",
+        description: "It has big buttery, mushroom notes, and though very rich, it's less creamy and runny than brie.",
+        rating: 4
+    },
+    {
+        type: "Aged Cheddar",
+        description: "Aged cheddars are crumbly and complex in flavor.",
+        rating: 2
+    },
+    {
+        type: "Young Cheddar",
+        description: "Like most cheeses, young cheddars are mild and creamy;",
+        rating: 3
+    },
+    {
+        type: "cotija",
+        description: " Cotija has a strong tangy flavor with a rich milky texture. It doesn't melt and has an adhesive quality, making it the perfect topping for elote.",
+        rating: 5
+    },
+]
